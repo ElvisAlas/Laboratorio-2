4a1c74722266d00270bd3c4ed213edbdff0b67ba
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2024-02-02 22:01:08 -0600

    creando las variables de entorno

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,19 @@
 import pg from 'pg-promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const user=process.env.USER
+const pass=process.env.PASS
+const dataBase= process.env.DB
+const server=process.env.SERVER
+const portDb=process.env.PORT_DB
+
+
 const pgp=pg()
-const cnstr = `postgresql://postgres:Ealas2201*@localhost:5432/rrhh`; 
+
+const cnstr = `postgresql://${user}:${pass}@${server}:${portDb}/${dataBase}`; 
+//const cnstr = `postgresql://postgres:Ealas2201*@localhost:5432/rrhh`; 
 const db = pgp(cnstr);
 
 db.connect()
