a7af19f08ccbb74c95e3576ea38dcbeb7161f3e8
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2024-02-02 22:10:37 -0600

    modificacion en el api

--- routers/apiRRHH.js
+++ routers/apiRRHH.js
@@ -3,7 +3,6 @@ import Express, { request } from "express";
 const empleado = Express();
 import { getEmpleado, postEmpleado,putEmpledo, deleteEmpleado} from "../controllers/empleadoscontroller.js";
 
-// import {getInfoEmpleado ,putSalario} from "../controllers/salariosController.js"
 
 empleado.get('', getEmpleado)
 
@@ -13,7 +12,7 @@ empleado.put('/:id', putEmpledo)
 
 empleado.delete('/:id', deleteEmpleado)
 
-// empleado.get('/RH', getInfoEmpleado)
 
-// empleado.put('RH/:idSalario', putSalario)
+
+
 export { empleado }
--- routers/apiSalarios.js
+++ routers/apiSalarios.js
@@ -5,7 +5,7 @@ const salario = Express();
 import {getsalario,getInfoEmpleado ,postSalario ,putSalario,deleteSalario} from "../controllers/salariosController.js"
 
 salario.get('/salario', getsalario)
-salario.get('', getInfoEmpleado)
+salario.get('/empleados', getInfoEmpleado)
 salario.post('', postSalario)
 salario.put('/:idSalario', putSalario)
 salario.delete('/:idSalario', deleteSalario)
